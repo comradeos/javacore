2a7f121c94daef342a5121e2f6abe28173b180b4
Iaroslav Os <osmnbx@gmail.com>
2022-07-24 11:11:03 +0300

    env test added

---
+++ EnvTest.java
@@ -0,0 +1,11 @@
+public class EnvTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! (Java)");
+    }
+}
+
+/*
+
+javac EnvTest.java && java EnvTest
+
+*/
