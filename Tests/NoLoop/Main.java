c4f1daf09ba22553965d66da6e85e1c4617edb6c
Iaroslav Os <osmnbx@gmail.com>
2023-01-09 09:59:40 +0200

    added/updated

--- Tests/NoLoop/Main.java
+++ Tests/NoLoop/Main.java
@@ -3,9 +3,9 @@ package Tests.NoLoop;
 public class Main {
     public static void main(String[] args) {
         String name = "Iaroslav";
-        String str = "xx";
-        str = str.replaceAll("x", "xx");
-        str = str.replaceAll("x", "xx");
+        String str = "xxxxxxxxxx";
+        str = str.replaceAll("x", "xxxxxxxxxx");
+        str = str.replaceAll("x", "xxxxxxxxxx");
         str = str.replaceAll("x", name + "\n");
         System.out.println(str);
     }
