d60d819a7c4b2d6fe0225f640c67ae697168e8c7
Iaroslav Os <osmnbx@gmail.com>
2023-06-05 22:08:52 +0200

    Update Main.java

--- Tests/ABCJava/src/Main.java
+++ Tests/ABCJava/src/Main.java
@@ -1,24 +1,19 @@
 interface MyInterface {
-    public static String name = null;
-    public static Integer age = null;
-
-    public static void showInfo()
-    {
-        System.out.println(name + age);
-    }
+    public String name = null;
+    public Integer age = null;
+    public static void showInfo() {}
 }
 
 class MyClass implements MyInterface {
-    public static String name = "Name";
-    public static Integer age = 123;
+    public String name = "Name";
+    public Integer age = 123;
+
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        MyClass myClass = new MyClass();
+
     }
 }
