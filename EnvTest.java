367be0d312ed39eccc886e8207624daa655deb9b
Iaroslav Os <osmnbx@gmail.com>
2024-01-22 19:28:16 +0200

    updated

--- EnvTest.java
+++ EnvTest.java
@@ -8,4 +8,15 @@ public class EnvTest {
 
 javac EnvTest.java && java EnvTest
 
+javac EnvTest.java
+
+jar cvfe EnvTest.jar EnvTest EnvTest.class
+
+c: Создает новый архив.
+v: Создает в консоли подробный вывод о файлах, которые добавляются в архив.
+f: Указывает, что имя архива будет следовать в командной строке (в данном случае, EnvTest.jar).
+e: Указывает точку входа для автономного приложения, содержащегося в JAR-файле, т.е. имя класса, в котором находится метод main (в данном случае, EnvTest).
+
+java -jar EnvTest.jar
+
 */
